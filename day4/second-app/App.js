34d84f7d6c7feac660ba70e97b79f903c21976ee
seonoh <eso0609@naver.com>
2019-09-24 19:26:34 +0900

    react-native navigation

---
+++ day4/second-app/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Seonoh Home Screen</Text>
+        <Button
+          title = 'Go detail screen'
+          onPress = {()=>this.props.navigation.push('Home')}/>
+        <Button
+          title = 'Go Back'
+          onPress = {()=>this.props.navigation.goBack()}/>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Seonoh Detail Screen</Text>
+        <Button
+          title = 'Go Home screen'
+          onPress = {()=>this.props.navigation.navigate('Home')}/>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default createAppContainer(AppNavigator);
